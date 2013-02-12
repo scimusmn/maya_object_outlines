c6e65b7ce2ec1d88a528b6cd50738505da62bcc7
Sarah German <sgerman@smm.org>
2013-02-12 13:38:24 -0600

    Moved slider code from plugin file to here

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+$(function() {
+  $('#slider').slider({
+    min: 0,
+    max: 1,
+    step: 0.01,
+    animate: 'slow',
+    slide: function(event, ui) {
+      $('#drawing').css('opacity', 1-ui.value);
+      $('#photo').css('opacity', ui.value);
+    }
+  });
+});
